b6a79eef961de7a286e98cdb03d9f21c476fb0bb
pdkary <pdkary@gmail.com>
2019-05-06 12:59:51 -0400

    Initialized Spring Project

---
+++ Agamotto/src/main/java/com/fiix/Agamotto/AgamottoApplication.java
@@ -0,0 +1,13 @@
+package com.fiix.Agamotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgamottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgamottoApplication.class, args);
+	}
+
+}
